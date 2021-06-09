5e8c7f6aca321a169b4c648689a0f6f2871c8901
Cristian Rodriguez <cristianccrj@gmail.com>
2021-06-08 20:35:56 -0500

    add function for screenshot fullpage and on webelement, and add screenshot in .gitignore

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -23,6 +23,9 @@ test('My first test cafe test', async t =>{
     const developer_name_input= Selector("#developer-name")
     const submit_button= Selector("#submit-button")
     const articleText = Selector("#article-header").innerText
+
+    await t.takeScreenshot({fullPage: true})
+    await t.takeElementScreenshot(submit_button)
     await t.typeText(developer_name_input,"Cristian")
     //await t.wait(3000)
     await t.click(submit_button)
