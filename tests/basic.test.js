7b0b992697972d5bf2787396768022d9ec718327
Cristian Rodriguez <cristianccrj@gmail.com>
2021-06-08 20:56:49 -0500

    add function for skip test and add disable cache

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -1,35 +1,49 @@
 import { Selector } from 'testcafe'
 
 //
-fixture `Getting started with testcafe`
-.page `https://devexpress.github.io/testcafe/example/`
-.before(async t =>{
-    //test setup goes here
-    //await rundatabasereset()
-    //await sendTestData()
-}).beforeEach(async t =>{
-    //Run before each test
-    await t.setTestSpeed(1)
-    //await test.timeouts(0)
+fixture`Getting started with testcafe`
+    .page`https://devexpress.github.io/testcafe/example/`
+    .before(async t => {
+        //test setup goes here
+        //await rundatabasereset()
+        //await sendTestData()
+    }).beforeEach(async t => {
+        //Run before each test
+        await t.setTestSpeed(1)
+        //await test.timeouts(0)
 
-}).after(async t =>{
-    //cleaning test data
-    // logging and seding data to monitoring systems
-}).afterEach(async t =>{
-    // Runs aftter eahc test
-})
+    }).after(async t => {
+        //cleaning test data
+        // logging and seding data to monitoring systems
+    }).afterEach(async t => {
+        // Runs aftter eahc test
+    })
 
 
-test('My first test cafe test', async t =>{
-    const developer_name_input= Selector("#developer-name")
-    const submit_button= Selector("#submit-button")
+test('My first test cafe test', async t => {
+    const developer_name_input = Selector("#developer-name")
+    const submit_button = Selector("#submit-button")
     const articleText = Selector("#article-header").innerText
 
-    await t.takeScreenshot({fullPage: true})
+    await t.takeScreenshot({ fullPage: true })
     await t.takeElementScreenshot(submit_button)
-    await t.typeText(developer_name_input,"Cristian")
+    await t.typeText(developer_name_input, "Cristian")
     //await t.wait(3000)
     await t.click(submit_button)
     //compare text of a webelement
     await t.expect(articleText).contains('Cristian')
 })
+
+/*test.only('My first test cafe test', async t => {
+    const developer_name_input = Selector("#developer-name")
+    const submit_button = Selector("#submit-button")
+    const articleText = Selector("#article-header").innerText
+
+    await t.takeScreenshot({ fullPage: true })
+    await t.takeElementScreenshot(submit_button)
+    await t.typeText(developer_name_input, "Cristian")
+    //await t.wait(3000)
+    await t.click(submit_button)
+    //compare text of a webelement
+    await t.expect(articleText).contains('Cristian')
+})*/
